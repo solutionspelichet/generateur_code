299a7b8f3791d52a96bcee059a90150bc1850791
Arnaud Guedou <arnaud.guedou@pelichet.ch>
2025-05-26 11:30:58 +0200

    Mise à jour app.py pour Render (port, host, etc.)

--- app.py
+++ app.py
@@ -180,5 +180,7 @@ def index():
 
     return render_template('index.html')
 
-print("Flask démarre...")
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Flask démarre sur le port {port}...")
+    app.run(host="0.0.0.0", port=port)
